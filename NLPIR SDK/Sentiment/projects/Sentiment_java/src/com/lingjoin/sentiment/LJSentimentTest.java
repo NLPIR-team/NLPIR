39e31a6b6e13c55f3b4581c768dec2f6126901d6
huoshan12345 <89009143@qq.com>
2015-08-04 09:30:56 +0800

    更新情感分析、新情感分析组件的相关文件

--- NLPIR SDK/Sentiment/projects/Sentiment_java/src/com/lingjoin/sentiment/LJSentimentTest.java
+++ NLPIR SDK/Sentiment/projects/Sentiment_java/src/com/lingjoin/sentiment/LJSentimentTest.java
@@ -1,29 +1,78 @@
 package com.lingjoin.sentiment;
 
+import org.junit.Test;
+
+
 public class LJSentimentTest {
-    public static void main(String[] args) {
-        System.out.println("初始化开始...");
-        if (CLibrarySentiment.Instance.ST_Init("", 1, "") == 0) {
-            System.out.println(CLibrarySentiment.Instance.ST_GetLastErrMsg());
-            System.out.println("初始化失败");
-            System.exit(1);
-        } else {
-            System.out.println("初始化成功");
-        }
+	
+	/**
+	 * 测试导出情感词典
+	 */
+	@Test
+	public void testExportDict() {
+		System.out.println("初始化开始...");
+		if ( !CLibrarySentiment.Instance.ST_Init("sentimentData", 1, "") ) {
+			System.out.println(CLibrarySentiment.Instance.ST_GetLastErrMsg());
+			System.out.println("初始化失败");
+			System.exit(1);
+		} else {
+			System.out.println("初始化成功");
+		}
+		//导出积极和消极情感词典
+		if ( CLibrarySentiment.Instance.ST_SentiDictIO("positive.txt", "negative.txt", false) == 1 ) {
+			System.out.println("词典导出成功，请到对应的目录查找");
+		} else {
+			System.out.println("词典导出失败");
+		}
+		
+		CLibrarySentiment.Instance.ST_Exit();
+	}
 
-        int num = CLibrarySentiment.Instance.ST_ImportUserDict("sentilexicon.txt", 0);
-        System.out.printf("添加用户单词%d个\n", num);
-
-        String sTitle = "奥巴马与美防长商打击IS战略 称不会“速战速决”";
-        String sContent = "据美媒报道，当地时间6日，美国总统奥巴马前往美国国防部，与国防部长卡特等商讨如何改善击败“伊斯兰国”武装分子的战略。奥巴马承认美国目前缺乏一个“完整战略”。并表示，打击“伊斯兰国”之战不会是“速战速决”据报道，美国领导的联军上周末对控制伊拉克西北部和叙利亚东部大片地区的激进分子进行了38次空袭，其中近一半的空袭目标锁定在“伊斯兰国”自行宣布的首都拉卡周围地区。美国领导的盟军去年8月和9月先后开始对伊拉克和叙利亚境内目标展开空袭。据美国国防部说，到目前为止，联军至少进行了4800次空袭，帮助伊拉克军队和叙利亚境内武装收复一些失地。奥巴马说，打击“伊斯兰国”极端分子的战斗将是长期而艰苦的，要想赢得胜利，需要的将不只是美国军事力量。奥巴马提到了持续的多边行动，包括联军部队从空中打击“伊斯兰国”，同时采取行动切断该组织的资金来源，并强化地方部队在伊拉克和其他地方展开地面行动。奥巴马指出，“伊斯兰国”主动接触包括在美国等遥远地方的潜在新兵，并表示击败“伊斯兰国”将不能只靠军事力量。据悉，联军去年八月在伊拉克展开空袭，一个月后扩展到叙利亚。根据白宫的数据，目前总的空袭次数已经超过5000次，官员们说空袭行动帮助了伊拉克部队和叙利亚战斗人员从激进分子手中收复了一些失地。报道称，但是进展一直是有限的，伊拉克军队仍然无法在夺取北部和西部城市的地面战斗中赢得大规模胜利。伊拉克部队在诸如争夺安巴尔省的战斗等行动中得到了伊朗支持的民兵和库尔德武装人员的帮助。到目前为止，奥巴马一直拒绝向伊拉克派遣美国地面部队的要求，而是派遣了顾问和培训人员来训练和加强伊拉克部队。";
-
-        System.out.println(CLibrarySentiment.Instance.ST_GetOneObjectResult(sTitle, sContent, "奥巴马"));
-        System.out.println(CLibrarySentiment.Instance.ST_GetMultiObjectResult(sTitle, sContent, "stConduct.xml"));
-        System.out.println(CLibrarySentiment.Instance.ST_GetSentencePoint(sContent));
-
-        CLibrarySentiment.Instance.ST_Exit();
-
-        System.exit(0);
-
-    }
+	/**
+	 * 测试导入情感词典
+	 */
+	@Test
+	public void testImportDict() {
+		System.out.println("初始化开始...");
+		if ( !CLibrarySentiment.Instance.ST_Init("sentimentData", 1, "") ) {
+			System.out.println(CLibrarySentiment.Instance.ST_GetLastErrMsg());
+			System.out.println("初始化失败");
+			System.exit(1);
+		} else {
+			System.out.println("初始化成功");
+		}
+		//导入积极和消极情感词典
+		if ( CLibrarySentiment.Instance.ST_SentiDictIO("test/pos.txt", "test/neg.txt", true) == 1 ) {
+			System.out.println("恭喜，词典导入成功");
+		} else {
+			System.out.println("词典导入失败");
+		}
+		
+		CLibrarySentiment.Instance.ST_Exit();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("初始化开始...");
+		if ( !CLibrarySentiment.Instance.ST_Init("sentimentData", 1, "") ) {
+			System.out.println(CLibrarySentiment.Instance.ST_GetLastErrMsg());
+			System.out.println("初始化失败");
+			System.exit(1);
+		} else {
+			System.out.println("初始化成功");
+		}
+		
+		
+		String sTitle = "郭德纲需反思：观众如水，能载舟亦能覆舟";
+		String sContent = "据《法制晚报》报道，警方已经证实1月9号在首都国际机场打人的均是德云社员工，且德云社三名员工因打人被警方处以行政拘留并罚款。目前德云社已申请行政复议，而郭德纲与“打人”字眼再次引起人们的热议。\n " +
+			  "德云社为什么会频频出现打人事件，这是一个让我们很难理解的事情。在舞台上，郭德纲把自己的身段放的那么低，与观众是那样的亲，为什么一到舞台下面就似乎完全是换了一个人呢?\n" + 
+			  "郭德纲不在体制内，按他的话说自己就是一个“非著名相声演员”，而那些在体质内的则统一被他戏称为“主流的”，而且在一切场合尽自己的最大的可能来讽刺和挖苦这些所谓的“主流”相声演员。郭德纲成名，靠的不是哪个政府部门，靠的是自己坚持不懈的努力，靠的是观众们的力捧，靠的是电视台网络的大力宣传。所以，他在唱经典段子《大实话》的时候会一直唱“要说亲，观众们亲，观众演员心连着心!”";
+		
+		System.out.println(CLibrarySentiment.Instance.ST_GetOneObjectResult(sTitle, sContent, "郭德纲"));
+		System.out.println("=====================================================================================");
+		System.out.println(CLibrarySentiment.Instance.ST_GetMultiObjectResult(sTitle, sContent, "stConduct.xml"));
+		
+		CLibrarySentiment.Instance.ST_Exit();
+		
+		System.exit(0);
+	}
 }
---
+++ NLPIR SDK/SentimentNew/sample/C#/SentimentNew.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lingjoin.SentimentNewTest
+{
+    public class SentimentNew : Lingjoin.Global.Base
+    {
+        public override string DllFileName
+        {
+            get { return "SentimentNew.dll"; }
+        }
+
+        public override string AuthorizationFileName
+        {
+            get { return "nlpir.user"; }
+        }
+
+        protected override bool InitFunctions()
+        {
+            d_ST_Init = (D_ST_Init)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_Init", typeof(D_ST_Init));
+
+            d_ST_GetOneObjectResult = (D_ST_GetOneObjectResult)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_GetOneObjectResult", typeof(D_ST_GetOneObjectResult));
+
+            d_ST_GetMultiObjectResult = (D_ST_GetMultiObjectResult)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_GetMultiObjectResult", typeof(D_ST_GetMultiObjectResult));
+
+            d_ST_GetLastErrMsg = (D_ST_GetLastErrMsg)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_GetLastErrMsg", typeof(D_ST_GetLastErrMsg));
+
+            d_ST_Exit = (D_ST_Exit)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_Exit", typeof(D_ST_Exit));
+
+            d_ST_ImportUserDict = (D_ST_ImportUserDict)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_ImportUserDict", typeof(D_ST_ImportUserDict));
+
+            d_ST_GetSentencePoint = (D_ST_GetSentencePoint)DllWrapper.GetFunctionAddress(
+                DllWrapper.hModule, "ST_GetSentencePoint", typeof(D_ST_GetSentencePoint));
+
+            //             d_ST_SentiDictIO = (D_ST_SentiDictIO)dllWrapper.GetFunctionAddress(
+            //                 dllWrapper.hModule, "ST_SentiDictIO", typeof(D_ST_SentiDictIO));
+
+            if (d_ST_Init == null || d_ST_GetOneObjectResult == null
+                || d_ST_GetMultiObjectResult == null || d_ST_GetLastErrMsg == null
+                || d_ST_Exit == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate bool D_ST_Init(String sInitDirPath, int encode, String sLicenseCode);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate IntPtr D_ST_GetOneObjectResult(String sTitle, String sContent, String sObject);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate IntPtr D_ST_GetMultiObjectResult(String sTitle, String sContent, String sObjectRuleFile);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate void D_ST_Exit();
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate IntPtr D_ST_GetLastErrMsg();
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate int D_ST_ImportUserDict(string sFilePath, int bSaveDict);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private delegate IntPtr D_ST_GetSentencePoint(string sSentence);
+
+        private D_ST_Init d_ST_Init;
+        private D_ST_GetOneObjectResult d_ST_GetOneObjectResult;
+        private D_ST_GetMultiObjectResult d_ST_GetMultiObjectResult;
+        private D_ST_Exit d_ST_Exit;
+        private D_ST_GetLastErrMsg d_ST_GetLastErrMsg;
+        // private D_ST_SentiDictIO d_ST_SentiDictIO;
+        private D_ST_ImportUserDict d_ST_ImportUserDict;
+        private D_ST_GetSentencePoint d_ST_GetSentencePoint;
+
+        public SentimentNew(params string[] otherDllNames) : base(otherDllNames) { }
+
+
+
+        public bool ST_Init(String sInitDirPath, int encode, String sLicenseCode)
+        {
+            return d_ST_Init(sInitDirPath, encode, sLicenseCode);
+        }
+
+        public String ST_GetOneObjectResult(String sTitle, String sContent, String sObject)
+        {
+            return Marshal.PtrToStringAnsi(d_ST_GetOneObjectResult(sTitle, sContent, sObject));
+        }
+
+        public String ST_GetMultiObjectResult(String sTitle, String sContent, String sObjectRuleFile)
+        {
+            return Marshal.PtrToStringAnsi(d_ST_GetMultiObjectResult(sTitle, sContent, sObjectRuleFile));
+        }
+
+        public void ST_Exit()
+        {
+            d_ST_Exit();
+        }
+
+        public String ST_GetLastErrMsg()
+        {
+            return Marshal.PtrToStringAnsi(d_ST_GetLastErrMsg());
+        }
+
+        public int ST_ImportUserDict(string sFilePath, int bSaveDict = 0)
+        {
+            return d_ST_ImportUserDict(sFilePath, bSaveDict);
+        }
+
+        public string ST_GetSentencePoint(string sSentence)
+        {
+            return Marshal.PtrToStringAnsi(d_ST_GetSentencePoint(sSentence));
+        }
+
+
+        /**
+         * 功能：批量导入导出情感词典
+         * @param sPositiveFile 积极词典文件名；
+         * @param sNegativeFile 消极词典文件名
+         * @param bInputOutput true: 导入词典到系统中； false:导出系统词典到文本文件
+         * @return int: 1:成功；0:失败
+         */
+        //         public int ST_SentiDictIO(String sPositiveFile, String sNegativeFile, bool bInputOutput)
+        //         {
+        //             return d_ST_SentiDictIO(sPositiveFile, sNegativeFile, bInputOutput);
+        //         }
+    }
+}
