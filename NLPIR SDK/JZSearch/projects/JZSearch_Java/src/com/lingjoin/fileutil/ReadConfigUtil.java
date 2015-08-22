abd5c7bbb5affd331248e4c93eb24dfbe709c8ce
Dr. Kevin Zhang <kevinzhang@bit.edu.cn>
2015-08-22 23:44:35 +0800

    JZSearch JAVA sample project

---
+++ NLPIR SDK/JZSearch/projects/JZSearch_Java/src/com/lingjoin/fileutil/ReadConfigUtil.java
@@ -0,0 +1,54 @@
+package com.lingjoin.fileutil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadConfigUtil {
+	private static Properties config = null;
+	static {
+		InputStream in = null;
+		try {
+			File config_file_path = new File("JZSearch.properties");
+			if (config_file_path.exists()) {
+				in = new FileInputStream(config_file_path);
+			} else {
+				in = ReadConfigUtil.class.getClassLoader().getResourceAsStream(
+						"JZSearch.properties");
+			}
+		} catch (FileNotFoundException e1) {
+			System.out.println("config file not found JZSearch.properties!");
+		}
+		config = new Properties();
+		try {
+			if(in==null){
+				System.out.println("config file not found JZSearch.properties!");
+			}else {
+				config.load(in);
+				in.close();				
+			}
+		} catch (IOException e) {
+			System.out.println("No JZSearch.properties defined error");
+		}
+	}
+
+	// 根据key读取value
+	public static String getValue(String key) {
+		// Properties props = new Properties();
+		try {
+			String value = config.getProperty(key);
+			return value;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("ConfigInfoError" + e.toString());
+			return null;
+		}
+	}
+
+	public static void main(String args[]) {
+		System.out.println(getValue("contentUrl"));
+	}
+}
