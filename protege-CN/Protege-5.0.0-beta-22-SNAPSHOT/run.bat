912776af0f25e758b686b785eb75e6b91c50d985
Hongyan Pan <gnail001@hotmail.com>
2018-03-08 20:00:33 +0800

    New updates

---
+++ protege-CN/Protege-5.0.0-beta-22-SNAPSHOT/run.bat
@@ -0,0 +1,1 @@
+java -Xmx500M -Xms200M -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath bundles/guava.jar;bundles/logback-classic.jar;bundles/logback-core.jar;bundles/slf4j-api.jar;bin/org.apache.felix.main.jar;bin/protege-launcher.jar org.protege.osgi.framework.Launcher %1
---
+++ protege-CN/Protege-5.0.0-beta-22-SNAPSHOT/run.command
@@ -0,0 +1,7 @@
+cd "$(dirname "$0")"
+
+CMD_OPTIONS="-Dapple.laf.useScreenMenuBar=true -Dcom.apple.mrj.application.apple.menu.about.name=Protege -Xdock:name=Protege -Xdock:icon=app/Protege.icns" sh run.sh $1
+
+
+
+
