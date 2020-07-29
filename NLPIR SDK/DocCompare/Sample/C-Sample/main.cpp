8bec33a23821a79a98e7fff74674f838f88926ec
drkevinzhang <kevinzhangbit.edu.cn>
2020-07-29 15:11:01 +0800

    Update DocCompare Library

---
+++ NLPIR SDK/DocCompare/Sample/C-Sample/main.cpp
@@ -0,0 +1,59 @@
+﻿#include "include/DocCompare.h"
+#include <string>
+#include <vector>
+#include <time.h>
+
+#ifdef WIN64
+#pragma comment(lib, "../../../bin/DocCompare/x64/DocCompare.lib")
+#else
+#pragma comment(lib, "../../../bin/DocCompare/DocCompare.lib")
+#endif
+int main()
+{
+    std::vector<std::string> filelist = {
+		"D:\\NLPIR\\test\\compare\\word.txt",
+        
+	};
+	std::vector<std::string> testfiles = 
+	{
+		"D:\\NLPIR\\test\\compare\\pdf.txt"
+	};
+
+    if(DC_Init("D:\\NLPIR")!=1)
+    {
+        printf("初始化失败!错误原因是：%s\n",DC_GetLastErrorMsg());
+        return -1;
+    }
+    printf("初始化成功！\n");
+    DC_HANDLE test = DC_NewInstance("./", false);//第一个参数为结果存放路径，第二个参数选择的是XML存储
+    double start = clock();
+    size_t length = filelist.size();
+    const bool isTestFile = true;
+	DC_Cmp2Files(test, testfiles[0].c_str(), filelist[0].c_str());//比较两个文件，比较结果存放在指定路径
+
+
+	printf("耗时：%.2fs\n", (clock() - start) / CLOCKS_PER_SEC);
+
+
+    for(size_t i = 0; i < length; i++)
+    {
+		if (DC_AddFile2Lib(test, filelist[i].c_str()))//加载待比较的文件进入模板库
+		{
+			printf("%s 加载完成(%zd)\n", filelist[i].c_str(), i + 1);
+		}
+		else
+		{
+			printf("%s 加载失败(X)\n", filelist[i].c_str());
+		}
+    }
+
+	DC_Cmp2Files(test, testfiles[0].c_str(), filelist[0].c_str());//将指定的文件与模板库大批量数据进行比较，得到结果存储在指定路径下
+
+
+    printf("耗时：%.2fs\n", (clock() - start) / CLOCKS_PER_SEC);
+	DC_DeleteInstance(test);
+    DC_Exit();
+
+    //system("pause");
+    return 0;
+}
--- NLPIR SDK/DocCompare/include/DocCompare.h
+++ NLPIR SDK/DocCompare/include/DocCompare.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 *
-* NLPIR Document Compare System Copyright (c) 2000-2019
-
+* NLPIR Document Compare System Copyright (c) 2000-2020
+*	  提供各类复杂的文档比较功能，生成比较结果的json格式串
 *     All rights reserved.
 *
 * This file is the confidential and proprietary property of
@@ -9,12 +9,14 @@
 * a written license from the author.
 * Filename:
 * Abstract:
-*          NLPIR.h: definition of the NLPIR lexical analysis system API
+*          DocCompare.h: definition of the NLPIR Document Compare System
+*
 * Date:     2019-8-9
 *
-* Notes:
+* Notes:  第一版本：实验室研究生王刚（wanggang@nlpir.org）
 *
 ****************************************************************************/
+#ifndef __NLPIR_DOC_COMPARE_INCLUDED_H___
 #ifdef OS_LINUX
 #define DOCCOMPARE_API extern "C" 
 #else
@@ -24,30 +26,157 @@
 #define DOCCOMPARE_API extern "C"  __declspec(dllimport)
 #endif
 #endif
-DOCCOMPARE_API int DocComp_Init(void);
 
+typedef size_t DC_HANDLE;//文本比较的运算HANDLE
 
-DOCCOMPARE_API void* DocComp_NewInstance(const char* outputDir, const char* exePath, bool useJson);
+/*********************************************************************
+*
+*  Func Name  : DC_Init
+*
+*  Description: Init DC
+*               The function must be invoked before any operation listed as following
+*
+*  Parameters : const char * sInitDirPath=NULL
+*				 sDataPath:  Path where Data directory stored.
+*				 the default value is NULL, it indicates the initial directory is current working directory path
+*				 encode: encoding code;
+*				 sLicenseCode: license code for unlimited usage. common user ignore it
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API int DC_Init(const char * sDataPath = 0, const char*sLicenceCode = 0);
+/*********************************************************************
+*
+*  Func Name  : DC_Exit
+*
+*  Description: Exit DC
+*               The function must be invoked before any operation listed as following
+*
+*  Parameters : void
+*  Returns    : void
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API void DC_Exit(void);
 
+/*********************************************************************
+*
+*  Func Name  : DC_NewInstance
+*
+*  Description: Init IE
+*               The function must be invoked before any operation listed as following
+*
+*  Parameters : const char* outputDir:  Path where Data directory stored.
+*				useJson: 是否使用Json格式;
+*  Returns    : DC_HANDLE, failed return size_t -1;
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API DC_HANDLE DC_NewInstance(const char* outputDir,  bool useJson=false);
 
-DOCCOMPARE_API void DocComp_Exit(void * pCompare);
+/*********************************************************************
+*
+*  Func Name  : DC_DeleteInstance
+*
+*  Description: Delete Instance
+*
+*  Parameters : DC_HANDLE：由DC_NewInstance生成的Handle
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API void DC_DeleteInstance(DC_HANDLE pDCHandle);
 
-DOCCOMPARE_API int DocComp_ImportTemplate(void *pCompare, const char* filePath);
+/*********************************************************************
+*
+*  Func Name  : DC_GetLastErrorMsg
+*
+*  Description: GetLastErrorMessage
+*
+*
+*  Parameters : const char* 返回的错误信息
+*
+*
+*  Returns    : the result buffer pointer
+*
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2014-2-27
+*********************************************************************/
+DOCCOMPARE_API const char * DC_GetLastErrorMsg();
 
-DOCCOMPARE_API const char * DocComp_compareDocFile(void *pCompare, const char* sTextFile);
+/*********************************************************************
+*
+*  Func Name  : DC_Cmp2Files
+*
+*  Description: 比较两个文档，结果采用json格式存储并返回
+*
+*  Parameters : DC_HANDLE：由DC_NewInstance生成的Handle
+*				const char* sFile1：比较的文件1
+*				const char *sFile1：比较的文件2
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API const char * DC_Cmp2Files(DC_HANDLE pDCHandle, const char* sFile1, const char *sFile2);
+//两个指定的文件之间进行比对;忽略范文查找过程 sCmpFile:比较文件；sBeCmpedFile:带比较的文件
+//added in 2019/8/12
 
-DOCCOMPARE_API const char * DocComp_compareDoc2Files(void *pCompare, const char* sCmpFile,const char *sBeCmpedFile);
+/*********************************************************************
+*
+*  Func Name  : DC_Cmp2Str
+*
+*  Description: 比较两个文档，结果采用json格式存储并返回
+*
+*  Parameters : DC_HANDLE：由DC_NewInstance生成的Handle
+*				const char* sStr1：比较的字符串1
+*				const char *sStr2：比较的字符串2
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API const char * DC_Cmp2Str(DC_HANDLE pDCHandle, const char* sStr1, const char *sStr2);
 //两个指定的文件之间进行比对;忽略范文查找过程 sCmpFile:比较文件；sBeCmpedFile:带比较的文件
 //added in 2019/8/12
 
 
-DOCCOMPARE_API void DocComp_setConvertexeDir(void *pCompare, const char* filePath);
+/*********************************************************************
+*
+*  Func Name  : DC_AddFile2Lib
+*
+*  Description: 添加待比较的文档进入模板文档库
+*
+*  Parameters : DC_HANDLE：由DC_NewInstance生成的Handle
+*				const char* sFilename：模板文件名
+*
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API int DC_AddFile2Lib(DC_HANDLE pDCHandle, const char* sFilename);
 
-DOCCOMPARE_API void DocComp_setOutputDir(void *pCompare, const char* filePath);
+/*********************************************************************
+*
+*  Func Name  : DC_CmpFile2Lib
+*
+*  Description: 添加待比较的文档进入模板文档库
+*
+*  Parameters : DC_HANDLE：由DC_NewInstance生成的Handle
+*				const char* sFilename：待比较的文本文件
+*
+*  Returns    : success or fail
+*  Author     : Kevin Zhang
+*  History    :
+*              1.create 2013-6-8
+*********************************************************************/
+DOCCOMPARE_API const char * DC_CmpFile2Lib(DC_HANDLE pDCHandle, const char* sFilename);
 
-DOCCOMPARE_API int DocComp_convert2TXT(void *pCompare, const char* filePath);
-
-DOCCOMPARE_API int DocComp_LoadFile(void *pCompare, const char* filePath, int isTest);
-DOCCOMPARE_API void DocComp_GenerateResult(void *pCompare);
-
-
+#endif // !__NLPIR_DOC_COMPARE_INCLUDED_H___
